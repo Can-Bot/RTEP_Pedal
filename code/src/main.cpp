72d39bd0257b5c271e6e2ca2cb9fff8f736061e4
Alex Lake <alexander.lake29@gmail.com>
2025-02-26 14:21:16 +0000

    Reads .WAV, created output, no pitch shifting

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,26 +1,83 @@
 #include <cstdio>
-// #include "Config.h"
-// #include "DisplayHandler.h"
-// #include "SamplingModule.h"
-// #include "DigitalSignalChain.h"
-// #include "OutputModule.h"
+#include <iostream>
+#include <vector>
+#include <sndfile.h>
+#include "../lib/stretch/signalsmith-stretch.h"
 
-int main()
-{
-    // Config config;
-    // DisplayHandler displayHandler;
-    // SamplingModule sampler;
-    // DigitalSignalChain dspChain;
-    // OutputModule output;
+int main() {
+    // WAV Setup
+    const char *inputFile = "/home/alake/RTEP_Pedal/code/input.wav";
+    SF_INFO sfInfo;
+    SNDFILE *inFile = sf_open(inputFile, SFM_READ, &sfInfo);
+    if (!inFile) {
+        std::cerr << "Error: Could not open input file!" << std::endl;
+        return -1;
+    }
+    if (sfInfo.channels < 1 || sfInfo.channels > 2) {
+        std::cerr << "Error: Only mono and stereo WAV files are supported!" << std::endl;
+        sf_close(inFile);
+        return -1;
+    }
+    const char *outputFile = "output.wav";
+    SNDFILE *outFile = sf_open(outputFile, SFM_WRITE, &sfInfo);
+    if (!outFile) {
+        std::cerr << "Error: Could not open output file!" << std::endl;
+        sf_close(inFile);
+        return -1;
+    }
 
-    // float sample;
-    // sampler.captureAudio(sample);
-    // dspChain.registerEffect(config.getEffect());
-    // dspChain.applyEffects(sample);
-    // output.outputSample(sample);
-    // displayHandler.updateDisplay(config);
+    const int bufferSize = 1024;
+    std::vector<float> inputBuffer(bufferSize * sfInfo.channels);
+    std::vector<float> outputBuffer(bufferSize * sfInfo.channels);
+    std::vector<float*> inputChannels(sfInfo.channels);
+    std::vector<float*> outputChannels(sfInfo.channels);
+    for (int c = 0; c < sfInfo.channels; ++c) {
+        inputChannels[c] = &inputBuffer[c];
+        outputChannels[c] = &outputBuffer[c];
+    }  
 
-    printf("Real Time Harmoniser Pedal\n");
+    // Pitch Shift
+    using Stretch = signalsmith::stretch::SignalsmithStretch<float>;
+    Stretch stretch;
+    stretch.setTransposeSemitones(4);  
+    while (true) {
+        int framesRead = sf_readf_float(inFile, inputBuffer.data(), bufferSize);
+        if (framesRead == 0) break;  // End of file
+        stretch.process(inputChannels.data(), framesRead, outputChannels.data(), framesRead);
+        sf_writef_float(outFile, outputBuffer.data(), framesRead);
+    }
 
+    sf_close(inFile);
+    sf_close(outFile);
+
+    std::cout << "Pitch shift completed - Output saved as 'output.wav'." << std::endl;
     return 0;
 }
+
+// int main()
+// {
+//     // Open input WAV file using libsndfile
+//     const char *inputFile = "input.wav";
+//     SF_INFO sfInfo;
+//     SNDFILE *inFile = sf_open(inputFile, SFM_READ, &sfInfo);
+//     if (!inFile) {
+//     std::cerr << "Error opening input file!" << std::endl;
+//     return -1;
+// }
+
+//     using Stretch = signalsmith::stretch::SignalsmithStretch<float>;
+//     Stretch stretch;
+//     stretch.presetDefault(channels, sampleRate);
+
+//     const int bufferSize = 1024;
+//     float inputBuffer[bufferSize];
+//     float outputBuffer[bufferSize];
+
+//     stretch.setTransposeSemitones(2); // up one semitone
+//     stretch.seek(inputBuffer, inputSample, playbackRateHint);
+//     stretch.flush(outputBuffer, outputSample);
+
+//     printf("Real Time Harmoniser Pedal\n");
+
+//     return 0;
+// }
